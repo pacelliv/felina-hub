06b015c000b647f3d1c64cb0953533b8530968d5
PacelliV <flores.eugenio03@gmail.com>
2023-04-05 15:48:35 -0500

    comments added and new syntax

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,35 +1,56 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-// Import styled components ServerStyleSheet
 import { ServerStyleSheet } from "styled-components"
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        // Step 1: Create an instance of ServerStyleSheet
-        const sheet = new ServerStyleSheet()
-        // Step 2: Retrieve styles from components in the page
-        const page = ctx.renderPage(
-            (App) => (props) => sheet.collectStyles(<App {...props} />)
-        )
-        // Step 3: Extract the styles as <style> tags
-        const styleTags = sheet.getStyleElement()
-        // Step 4: Pass styleTags as a prop
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps, ...page, styleTags }
-    }
+function MyDocument() {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
 
-    render() {
-        return (
-            <Html lang="eng">
-                <Head>
-                    {/* <title>My page</title> */}
-                    {/* Step 5: Output the styles in the head  */}
-                    {this.props.styleTags}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
+// Being a CSS-in-JavaScript styling, styled-components is geared for client-side rendering,
+// it normally assumes that's it's executing on the browser therefore it produces CSS styles
+// as a result of JavaScript execution and injects them directly into the document. Next JS
+// pre-renders all pages by default on the server, for this reason you need to collect the CSS
+// styles and include them in the server-rendered HTML response to avoid possible build issues
+// e.g. flashes of unstyled content.
+
+// To Address this issue we can use `ServerStyleSheet` from `styled-components` to collect all
+// the CSS styles used in the aplication to include them server-rendered HTML response.
+MyDocument.getInitialProps = async (ctx) => {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet()
+    // Step 2: Extract `renderPage` from the context
+    const originalRenderPage = ctx.renderPage
+
+    try {
+        // Step 3: Retrieve styles from components in the page
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) =>
+                    // Step 4: Extract the styles as <style> tags
+                    sheet.collectStyles(<App {...props} />),
+            })
+        // Step 5: Enable Server side rendering and do initial data population
+        const initialProps = await Document.getInitialProps(ctx)
+        // Step 6: Pass styles as a prop
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {sheet.getStyleElement()}
+                </>
+            ),
+        }
+    } finally {
+        sheet.seal()
     }
 }
+
+export default MyDocument
